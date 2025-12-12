5d4ae598b7969900a1f6708a089461974767d6df
ayemish <ayemish123@gmail.com>
2025-12-12 12:59:44 +0500

    Update API base URL to use environment variable for deployment

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,12 @@
+// src/api/api.js
+
 import axios from 'axios';
 
+
+const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL || 'http://127.0.0.1:8000'; 
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', 
+  baseURL: BASE_URL, 
 });
 
 export default api;
